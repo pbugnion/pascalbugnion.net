65c98b4b09bee2de4dc41f70cc090af21d5e1c38
Pascal Bugnion <pascal@bugnion.org>
2021-01-16 17:06:21 +0000

    add blog navbar

--- src/templates/blog-post.tsx
+++ src/templates/blog-post.tsx
@@ -5,22 +5,39 @@ import Layout from "../components/layout"
 
 import styles from "./blog.module.css"
 
-export default ({data}) => {
+export default ({ data }) => {
     return (
         <Layout>
-            <div className={`container ${styles.articleContainer}`}>
-                <header>
-                    <div className="page-header">
-                        <h1 className={styles.articleEntryTitle}>{data.markdownRemark.frontmatter.title}</h1>
-                        <div className={styles.lastUpdatedDate}>
-                            Last updated on the {data.markdownRemark.frontmatter.lastUpdatedDate}.
+            <header>
+                <nav className="navbar navbar-expand-md navbar-light fixed-top" id="top-navbar">
+                    <div className={`container ${styles.blogNavbarContainer}`}>
+                        <a className="navbar-brand" href="/index.html">Pascal Bugnion</a>
+                        <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbar-navigation">
+                            <span className="navbar-toggler-icon"></span>
+                        </button>
+                        <div className="collapse navbar-collapse" id="navbar-navigation">
+                            <ul className="navbar-nav">
+                                <li className="nav-item"><a href="code.html" className="nav-link">Code</a></li>
+                                <li className="nav-item"><a href="talks.html" className="nav-link">Talks</a></li>
+                                <li className="nav-item"><a href="http://www.scala4datascience.com" className="nav-link">Book</a></li>
+                                <li className="nav-item"><a href="blog/index.html" className="nav-link">Blog</a></li>
+                                <li className="nav-item active"><a href="#" className="nav-link">About</a></li>
+                            </ul>
                         </div>
                     </div>
-                </header>
-                <main>
+                </nav>
+            </header>
+            <main>
+                <div className={`container ${styles.articleContainer}`}>
+                    <div className="page-header">
+                        <h1 className={styles.articleEntryTitle}>{data.markdownRemark.frontmatter.title}</h1>
+                    </div>
+                    <div className={styles.lastUpdatedDate}>
+                        Last updated on the {data.markdownRemark.frontmatter.lastUpdatedDate}.
+                        </div>
                     <div className={styles.articleEntryContent} dangerouslySetInnerHTML={{ __html: data.markdownRemark.html }} />
-                </main>
-            </div>
+                </div>
+            </main>
         </Layout>
     )
 }
