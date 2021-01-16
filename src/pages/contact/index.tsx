b70c6a4639b057e8093c2f396af902c437cc32a0
Pascal Bugnion <pascal@bugnion.org>
2021-01-16 12:04:51 +0000

    Reformat contact page

--- src/pages/contact/index.tsx
+++ src/pages/contact/index.tsx
@@ -14,7 +14,7 @@ import githubIcon from "./images/github-icon.png"
 import twitterIcon from "./images/twitter-icon.png"
 import stackOverflowIcon from "./images/stackoverflow-icon.png"
 
-const ContactEntryLink = ({iconContent, href, title, value}) => {
+const ContactEntryLink = ({ iconContent, href, title, value }) => {
   return (
     <p className="contact-row">
       <OverlayTrigger
@@ -49,31 +49,31 @@ const ContactEntryEmail = () => {
 const IndexPage = () => {
   return (
     <>
-    <header>
-      <nav className="navbar navbar-expand-md navbar-light fixed-top" id="top-navbar">
-      <div className="container">
-        <a className="navbar-brand" href="/index.html">Pascal Bugnion</a>
-        <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbar-navigation">
-          <span className="navbar-toggler-icon"></span>
-        </button>
-        <div className="collapse navbar-collapse" id="navbar-navigation">
-          <ul className="navbar-nav">
-            <li className="nav-item"><a href="code.html" className="nav-link">Code</a></li>
-            <li className="nav-item"><a href="talks.html" className="nav-link">Talks</a></li>
-            <li className="nav-item"><a href="http://www.scala4datascience.com" className="nav-link">Book</a></li>
-            <li className="nav-item"><a href="blog/index.html" className="nav-link">Blog</a></li>
-            <li className="nav-item active"><a href="#" className="nav-link">About</a></li>
-          </ul>
-        </div>
-      </div>
-    </nav>
-  </header>
-  <main>
-    <div className="container content-container">
-      <div className="page-header">
-        <h1>About me</h1>
-      </div>
-      <div className="row">
+      <header>
+        <nav className="navbar navbar-expand-md navbar-light fixed-top" id="top-navbar">
+          <div className="container">
+            <a className="navbar-brand" href="/index.html">Pascal Bugnion</a>
+            <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbar-navigation">
+              <span className="navbar-toggler-icon"></span>
+            </button>
+            <div className="collapse navbar-collapse" id="navbar-navigation">
+              <ul className="navbar-nav">
+                <li className="nav-item"><a href="code.html" className="nav-link">Code</a></li>
+                <li className="nav-item"><a href="talks.html" className="nav-link">Talks</a></li>
+                <li className="nav-item"><a href="http://www.scala4datascience.com" className="nav-link">Book</a></li>
+                <li className="nav-item"><a href="blog/index.html" className="nav-link">Blog</a></li>
+                <li className="nav-item active"><a href="#" className="nav-link">About</a></li>
+              </ul>
+            </div>
+          </div>
+        </nav>
+      </header>
+      <main>
+        <div className="container content-container">
+          <div className="page-header">
+            <h1>About me</h1>
+          </div>
+          <div className="row">
             <div className="col-md-7 col-lg-8 contact-text">
               <p>
                 I am a software engineering technical lead.
@@ -81,32 +81,32 @@ const IndexPage = () => {
               <p>
                 I build tools for data science and machine learning. I live in London and work at <a href="https://faculty.ai/">Faculty</a>, as part of the <a href="https://faculty.ai/products-services/platform/">Faculty Platform</a> team.
                 </p>
-                <p>
-                  Outside of my day job, I am a contributor to open source, specially in the Jupyter and Plotly ecosystems. I am a core contributor to <a href="https://jupyter.org/widgets">Jupyter widgets</a> and the main author of <a href="/code.html">several Python libraries</a>.
+              <p>
+                Outside of my day job, I am a contributor to open source, specially in the Jupyter and Plotly ecosystems. I am a core contributor to <a href="https://jupyter.org/widgets">Jupyter widgets</a> and the main author of <a href="/code.html">several Python libraries</a>.
                 </p>
-                <p>
-                  I hold a PhD from Cambridge University in <a href="https://www.tcm.phy.cam.ac.uk/people/staff.html">theoretical solid state physics</a>, working on <a href="https://en.wikipedia.org/wiki/Quantum_Monte_Carlo">quantum Monte Carlo</a> methods.
+              <p>
+                I hold a PhD from Cambridge University in <a href="https://www.tcm.phy.cam.ac.uk/people/staff.html">theoretical solid state physics</a>, working on <a href="https://en.wikipedia.org/wiki/Quantum_Monte_Carlo">quantum Monte Carlo</a> methods.
                 </p>
 
             </div>
             <div className="col-md-5 col-lg-4 order-first order-md-last headshot-img-container">
-              <img src={headshot} alt="Pascal Bugnion" width="100%"/>
+              <img src={headshot} alt="Pascal Bugnion" width="100%" />
             </div>
-        </div>
-        <div className="row contact-links-row">
-          <div className="col-md-7 col-lg-8">
-            <ContactEntryEmail />
-            <ContactEntryLink iconContent={linkedInIcon} title="LinkedIn" href="https://uk.linkedin.com/in/pbugnion" value="pbugnion" />
-            <ContactEntryLink iconContent={githubIcon} title="GitHub" href="https://github.com/pbugnion" value="pbugnion" />
           </div>
-          <div className="col-md-5 col-lg-3">
-            <ContactEntryLink iconContent={twitterIcon} title="Twitter" href="https://twitter.com/pascalbugnion" value="@pascalbugnion" />
-            <ContactEntryLink iconContent={stackOverflowIcon} title="Stack Overflow" href="https://stackoverflow.com/users/827862/pascal-bugnion" value="pascal-bugnion" />
+          <div className="row contact-links-row">
+            <div className="col-md-7 col-lg-8">
+              <ContactEntryEmail />
+              <ContactEntryLink iconContent={linkedInIcon} title="LinkedIn" href="https://uk.linkedin.com/in/pbugnion" value="pbugnion" />
+              <ContactEntryLink iconContent={githubIcon} title="GitHub" href="https://github.com/pbugnion" value="pbugnion" />
+            </div>
+            <div className="col-md-5 col-lg-3">
+              <ContactEntryLink iconContent={twitterIcon} title="Twitter" href="https://twitter.com/pascalbugnion" value="@pascalbugnion" />
+              <ContactEntryLink iconContent={stackOverflowIcon} title="Stack Overflow" href="https://stackoverflow.com/users/827862/pascal-bugnion" value="pascal-bugnion" />
+            </div>
           </div>
         </div>
-    </div>
-    </main>
-  </>
+      </main>
+    </>
   )
 }
 
