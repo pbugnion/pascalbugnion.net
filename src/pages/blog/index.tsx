3960684111b32b4156ef5b888de3089a59d8d23e
Pascal Bugnion <pascal@bugnion.org>
2021-01-17 09:02:12 +0000

    Move navbar to separate component

--- src/pages/blog/index.tsx
+++ src/pages/blog/index.tsx
@@ -1,8 +1,10 @@
 import * as React from "react"
 import { Helmet } from "react-helmet"
 
-import Layout from "../../components/layout"
 import { graphql } from "gatsby"
+
+import Layout from "../../components/layout"
+import Navbar from "../../components/navbar"
 
 import styles from "./index.module.css"
 
@@ -27,30 +29,13 @@ const PostCard = ({post}) => {
 
 export default ({ data }) => {
     const posts = data.allMarkdownRemark.nodes.map(post => post.frontmatter)
-    console.log(posts)
     return (
         <Layout>
             <Helmet>
                 <title>Data diversions | Pascal Bugnion's blog</title>
             </Helmet>
             <header>
-                <nav className="navbar navbar-expand-md navbar-light fixed-top" id="top-navbar">
-                    <div className="container">
-                        <a className="navbar-brand" href="/index.html">Pascal Bugnion</a>
-                        <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbar-navigation">
-                            <span className="navbar-toggler-icon"></span>
-                        </button>
-                        <div className="collapse navbar-collapse" id="navbar-navigation">
-                            <ul className="navbar-nav">
-                                <li className="nav-item"><a href="code.html" className="nav-link">Code</a></li>
-                                <li className="nav-item"><a href="talks.html" className="nav-link">Talks</a></li>
-                                <li className="nav-item"><a href="http://www.scala4datascience.com" className="nav-link">Book</a></li>
-                                <li className="nav-item"><a href="blog/index.html" className="nav-link">Blog</a></li>
-                                <li className="nav-item active"><a href="#" className="nav-link">About</a></li>
-                            </ul>
-                        </div>
-                    </div>
-                </nav>
+                <Navbar containerAdditionalStyles={[styles.navbarContainer]}/>
             </header>
             <main className={styles.postListBackground}>
                 <div className={`container ${styles.articleIndexContainer}`}>
