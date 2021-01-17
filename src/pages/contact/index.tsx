4c07e287b311cfd40fca2f1a0f82c31ffa53e3fd
Pascal Bugnion <pascal@bugnion.org>
2021-01-17 09:07:47 +0000

    Use navbar component

--- src/pages/contact/index.tsx
+++ src/pages/contact/index.tsx
@@ -4,6 +4,7 @@ import Tooltip from 'react-bootstrap/Tooltip'
 import OverlayTrigger from 'react-bootstrap/OverlayTrigger'
 
 import Layout from "../../components/layout"
+import Navbar from "../../components/navbar"
 
 import headshot from "./images/Headshot-Pascal-1.jpg"
 import mailIcon from "./images/mail-icon.png"
@@ -50,23 +51,7 @@ const IndexPage = () => {
   return (
     <Layout>
       <header>
-        <nav className="navbar navbar-expand-md navbar-light fixed-top" id="top-navbar">
-          <div className={`container ${styles.maxWidthContainer}`}>
-            <a className="navbar-brand" href="/index.html">Pascal Bugnion</a>
-            <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbar-navigation">
-              <span className="navbar-toggler-icon"></span>
-            </button>
-            <div className="collapse navbar-collapse" id="navbar-navigation">
-              <ul className="navbar-nav">
-                <li className="nav-item"><a href="code.html" className="nav-link">Code</a></li>
-                <li className="nav-item"><a href="talks.html" className="nav-link">Talks</a></li>
-                <li className="nav-item"><a href="http://www.scala4datascience.com" className="nav-link">Book</a></li>
-                <li className="nav-item"><a href="blog/index.html" className="nav-link">Blog</a></li>
-                <li className="nav-item active"><a href="#" className="nav-link">About</a></li>
-              </ul>
-            </div>
-          </div>
-        </nav>
+        <Navbar containerAdditionalStyles={[styles.maxWidthContainer]} />
       </header>
       <main>
         <div className={`container content-container ${styles.maxWidthContainer}`}>
--- src/templates/blog-post/index.tsx
+++ src/templates/blog-post/index.tsx
@@ -3,6 +3,7 @@ import { graphql } from "gatsby"
 import { Helmet } from "react-helmet"
 
 import Layout from "../../components/layout"
+import Navbar from "../../components/navbar"
 
 import styles from "./index.module.css"
 
@@ -15,23 +16,7 @@ export default ({ data }) => {
                 <meta name="description" content={frontmatter.summary} />
             </Helmet>
             <header>
-                <nav className="navbar navbar-expand-md navbar-light fixed-top" id="top-navbar">
-                    <div className={`container ${styles.blogNavbarContainer}`}>
-                        <a className="navbar-brand" href="/index.html">Pascal Bugnion</a>
-                        <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbar-navigation">
-                            <span className="navbar-toggler-icon"></span>
-                        </button>
-                        <div className="collapse navbar-collapse" id="navbar-navigation">
-                            <ul className="navbar-nav">
-                                <li className="nav-item"><a href="code.html" className="nav-link">Code</a></li>
-                                <li className="nav-item"><a href="talks.html" className="nav-link">Talks</a></li>
-                                <li className="nav-item"><a href="http://www.scala4datascience.com" className="nav-link">Book</a></li>
-                                <li className="nav-item"><a href="blog/index.html" className="nav-link">Blog</a></li>
-                                <li className="nav-item active"><a href="#" className="nav-link">About</a></li>
-                            </ul>
-                        </div>
-                    </div>
-                </nav>
+                <Navbar containerAdditionalStyles={[styles.blogNavbarContainer]} />
             </header>
             <main>
                 <div className={`container ${styles.articleContainer}`}>
