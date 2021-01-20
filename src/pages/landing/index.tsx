61983ff917431b9a13255b257c693639697c575f
Pascal Bugnion <pascal@bugnion.org>
2021-01-20 08:34:46 +0000

    Add navbar

--- src/pages/landing/index.tsx
+++ src/pages/landing/index.tsx
@@ -2,13 +2,39 @@ import * as React from "react"
 
 import { Link } from "gatsby"
 
+import Navbar from "react-bootstrap/Navbar";
+import Nav from "react-bootstrap/Nav";
+
 import styles from "./index.module.css"
+
+const NavbarInternalLink = ({to, children}) => (
+    <Nav.Link as={Link} to={to} className={styles.landingNavLink}>{children}</Nav.Link>
+)
+
+const LandingNavbar = () => {
+    return (
+        <Navbar expand="sm" id={styles.landingNavbar}>
+            <Navbar.Brand href="/" className={styles.landingNavbarBrand}>Pascal Bugnion</Navbar.Brand>
+            <Navbar.Toggle aria-controls="basic-navbar-collapse-toggle" />
+            <Navbar.Collapse id="landing-navbar-collapse-toggle" className="justify-content-end">
+                <Nav>
+                    <NavbarInternalLink to="/code">Code</NavbarInternalLink>
+                    <Nav.Link href="http://www.scala4datascience.com/" className={styles.landingNavLink}>Book</Nav.Link>
+                    <NavbarInternalLink to="/blog">Blog</NavbarInternalLink>
+                    <NavbarInternalLink to="/contact">About</NavbarInternalLink>
+                </Nav>
+            </Navbar.Collapse>
+        </Navbar>
+    )
+}
 
 export default () => {
     return (
         <div className={styles.backgroundImage}>
             <div className={styles.contentContainer}>
                 <div className={styles.inside}>
+                    <LandingNavbar />
+                    <div className={styles.topSpacer}></div>
                     <h1 className={styles.lead}>
                         Hi. I&#8217;m Pascal.
                     </h1>
@@ -21,6 +47,7 @@ export default () => {
                             I also sometimes <Link to="/blog">write</Link> about data science, software, and leading software teams.
                         </p>
                     </div>
+                    <div className={styles.bottomSpacer}></div>
                 </div>
             </div>
         </div>
