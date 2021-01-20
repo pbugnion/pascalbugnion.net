730b7e6fa01ec3286ea974c0aa6516ffb105b047
Pascal Bugnion <pascal@bugnion.org>
2021-01-20 06:57:11 +0000

    Add links to other parts of site

--- src/pages/landing/index.tsx
+++ src/pages/landing/index.tsx
@@ -1,5 +1,7 @@
 import * as React from "react"
 
+import { Link } from "gatsby"
+
 import styles from "./index.module.css"
 
 export default () => {
@@ -11,8 +13,13 @@ export default () => {
                         Hi. I&#8217;m Pascal.
                     </h1>
                     <div className={styles.description}>
-                        <p className={styles.descriptionParagraph}>I'm a technical architect. I build tools for data scientists.</p>
-                        <p className={styles.descriptionParagraph}>This is my site. It is a repository for talks and software I have written and a blog about data engineering, maths, and software development.</p>
+                        <p className={styles.descriptionParagraph}>
+                            I'm a technical lead at <a href="https://faculty.ai">Faculty</a>. I build tools for data scientists.
+                        </p>
+                        <p className={styles.descriptionParagraph}>
+                            This is my site. It has some information <Link to="/contact">about me</Link> and <Link to="/code">projects</Link> I work on, both at work in my spare time. 
+                            I also sometimes <Link to="/blog">write</Link> about data science, software, and leading software teams.
+                        </p>
                     </div>
                 </div>
             </div>
