3fbc98327815acd8966b65ebae3f47b0689a3fd2
Pascal Bugnion <pascal@bugnion.org>
2021-01-23 09:07:43 +0000

    Fix styles on background when longer than screen

--- src/components/canvas-background/index.tsx
+++ src/components/canvas-background/index.tsx
@@ -2,7 +2,11 @@ import * as React from "react"
 
 import styles from "./index.module.css"
 
-export default ({children}) => (
+export type CanvasBackgroundProps = {
+    children: React.ReactNode
+}
+
+export default ({children}: CanvasBackgroundProps) => (
     <div className={styles.postListBackground}>
         {children}
     </div>
