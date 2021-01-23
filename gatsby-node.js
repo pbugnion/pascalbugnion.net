c45e813aa7ada5e3438aa8ba0c49922332a42c95
Pascal Bugnion <pascal@bugnion.org>
2021-01-23 16:57:52 +0000

    Add redirects for old blog post URLs

--- gatsby-node.js
+++ gatsby-node.js
@@ -28,6 +28,38 @@ const createBlogPages = async ({ graphql, actions }) => {
   });
 };
 
+const createHtmlRedirects = async({ actions }) => {
+  const baseRules = [
+    ["/code.html", "/code/"],
+    ["/contact.html", "/contact/"]
+  ]
+  const blogRules = [
+    ["editor-hacking-how-i-learnt-to-stop-worrying-and-love-my-emacs-configuration.html", "editor-hacking"],
+    ["flow-types-for-generators-and-coroutines.html", "flow-typing-generators-coroutines"],
+    ["how-frustrating-is-your-programming-language.html", "frustrating-programming-language"],
+    ["making-yourself-useful-as-a-middle-manager.html", "making-yourself-useful-as-a-middle-manager"],
+    ["managing-multiple-aws-credentials-with-pass.html", "managing-aws-credentials-with-pass"],
+    ["scala-in-production-four-conventions-for-safer-programs.html", "scala-in-production"],
+    ["scala-in-production-making-your-codebase-more-approachable.html", "scala-in-production-2"],
+    ["scraping-apis-with-akka-streams-part-2.html", "scraping-apis-with-akka-streams-2"],
+    ["scraping-apis-with-akka-streams.html", "scraping-apis-with-akka-streams"],
+    ["web-apis-with-scala-and-plotly.html", "plotly-as-lightweight-database"]
+  ]
+
+  const {createRedirect} = actions
+
+  baseRules.forEach(([fromPath, toPath]) => {
+    createRedirect({fromPath, toPath, isPermanent: true})
+  })
+
+  blogRules.forEach(([fromFragment, toFragment]) => {
+    const fromPath = `/blog/${fromFragment}`
+    const toPath = `/blog/${toFragment}`
+    createRedirect({fromPath, toPath, isPermanent: true})
+  })
+}
+
 exports.createPages = async ({ graphql, actions }) => {
+  await createHtmlRedirects({actions})
   await createBlogPages({ graphql, actions });
 };
