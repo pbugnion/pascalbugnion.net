78ca3fa1f33fd0aa1003d300e5bc2c2bdfa34dad
Pascal Bugnion <pascal@bugnion.org>
2021-01-30 15:36:39 +0000

    Use react-bootstrap on navbar
    Allows collapse to work.

--- src/components/navbar/index.tsx
+++ src/components/navbar/index.tsx
@@ -2,35 +2,37 @@ import * as React from "react"
 
 import { Link } from "gatsby"
 
+import Container from "react-bootstrap/Container"
+import Navbar from "react-bootstrap/Navbar"
+import Nav from "react-bootstrap/Nav"
+
 import styles from "./index.module.css"
 
 export interface NavbarProps {
     containerAdditionalStyles: Array<string>
 }
 
-const Navbar = ({ containerAdditionalStyles }) => {
+const SiteNavbar = ({ containerAdditionalStyles }) => {
     return (
-        <nav className={`navbar navbar-expand-md navbar-light fixed-top`} id={styles.topNavbar}>
-            <div className={["container", ...containerAdditionalStyles].join(" ")}>
-                <Link className="navbar-brand" to="/">Pascal Bugnion</Link>
-                <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbar-navigation">
-                    <span className="navbar-toggler-icon"></span>
-                </button>
-                <div className="collapse navbar-collapse" id="navbar-navigation">
+        <Navbar expand="sm" variant="light" fixed="top" id={styles.topNavbar}>
+            <Container className={containerAdditionalStyles.join(" ")}>
+                <Navbar.Brand as={Link} to="/">Pascal Bugnion</Navbar.Brand>
+                <Navbar.Toggle aria-controls="basic-navbar-collapse-toggle" />
+                <Navbar.Collapse id="basic-navbar-collapse-toggle">
                     <ul className="navbar-nav">
                         <li className="nav-item"><Link to="/code" className="nav-link">Code</Link></li>
                         <li className="nav-item"><a href="http://www.scala4datascience.com" className="nav-link">Book</a></li>
                         <li className="nav-item"><Link to="/blog" className="nav-link">Blog</Link></li>
                         <li className="nav-item"><Link to="/contact" className="nav-link">About</Link></li>
                     </ul>
-                </div>
-            </div>
-        </nav>
+                </Navbar.Collapse>
+            </Container>
+        </Navbar>
     )
 }
 
-Navbar.defaultProps = {
+SiteNavbar.defaultProps = {
     containerAdditionalStyles: []
 }
 
-export default Navbar
+export default SiteNavbar
