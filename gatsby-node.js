b86e4e14d40e940e4b6e4a4c02a90367af217c18
Pascal Bugnion <pascal@bugnion.org>
2021-05-15 17:13:08 +0100

    Add ability to find inbound and outbound references

--- gatsby-node.js
+++ gatsby-node.js
@@ -1,4 +1,5 @@
 const path = require("path");
+const { GraphQLString, GraphQLList } = require("gatsby/graphql")
 
 const createNotesPages = async ({ graphql, actions }) => {
   const { createPage } = actions;
@@ -73,3 +74,84 @@ exports.createPages = async ({ graphql, actions }) => {
   await createRedirectAboutContact({actions})
   await createNotesPages({ graphql, actions });
 };
+
+function findInMarkdown(markdown, regex) {
+  const unique = new Set();
+
+  let match;
+  while ((match = regex.exec(markdown))) {
+    const [, name] = match;
+    if (name) {
+      unique.add(name);
+    }
+  }
+
+  return Array.from(unique);
+}
+
+REGEX_FENCED_CODE_BLOCK = /^( {0,3}|\t)```[^`\r\n]*$[\w\W]+?^( {0,3}|\t)``` *$/gm;
+
+
+function cleanupMarkdown(markdown) {
+  const replacer = (foundStr) => foundStr.replace(/[^\r\n]/g, "");
+  return markdown
+    .replace(REGEX_FENCED_CODE_BLOCK, replacer) //// Remove fenced code blocks
+    .replace(/<!--[\W\w]+?-->/g, replacer) //// Remove comments
+    .replace(/^---[\W\w]+?(\r?\n)---/, replacer); //// Remove YAML front matter
+}
+
+const getReferences = (string) => {
+  const md = cleanupMarkdown(string);
+
+  const references = findInMarkdown(
+    md,
+    new RegExp("\\[[^\\]]+\\]\\(([^\\)]+)\\)", "ig")
+  )
+
+  return references;
+};
+
+exports.createResolvers = ({ createResolvers }) => {
+  const resolvers = {
+    Mdx: {
+      OutboundReferences: {
+        type: "[Mdx!]!",
+        resolve(source, args, context, info) {
+          const allReferences = getReferences(source.internal.content)
+          const internalReferences = allReferences.filter(
+            ref => ref.startsWith("/notes/"))
+          const res = context.nodeModel.runQuery({
+            query: {
+              filter: {
+                frontmatter: {
+                  slug: { in: internalReferences }
+                }
+              }
+            },
+            type: "Mdx",
+            firstOnly: false
+          })
+          return res.then(posts => 
+            posts === null ? [] : posts
+          )
+        }
+      },
+      InboundReferences: {
+        type: "[Mdx!]!",
+        resolve(source, args, context, info) {
+          const allNodes = context.nodeModel.getAllNodes({ type: "Mdx" })
+          allRelevantNodes = allNodes.filter(node => {
+            cleanedMarkdown = cleanupMarkdown(node.internal.content)
+            const reg = new RegExp(`\\[[^\\]]+\\]\\(${source.frontmatter.slug}\\)`, "ig")
+            console.log(reg)
+            const results = findInMarkdown(cleanedMarkdown, reg)
+            console.log(results)
+            return results ? true : false;
+          })
+          return allRelevantNodes
+        }
+      }
+    }
+  }
+  createResolvers(resolvers)
+}
