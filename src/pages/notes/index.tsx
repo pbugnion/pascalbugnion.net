2475b02e06029a9cc802c19107bc676105932946
Pascal Bugnion <pascal@bugnion.org>
2021-06-27 08:51:35 +0100

    Pin max navbar width to 960px

--- src/pages/notes/index.tsx
+++ src/pages/notes/index.tsx
@@ -15,7 +15,7 @@ export default ({ data }) => {
   return (
     <Layout>
       <header>
-        <Navbar containerAdditionalStyles={[]} />
+        <Navbar containerAdditionalStyles={[styles.maxWidthContainer]} />
       </header>
       <main>
         <div className={`container content-container ${styles.maxWidthContainer}`}>
