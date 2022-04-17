948f10df261c8595141d794c9f7b6da7804c4fa6
Pascal Bugnion <pascal@bugnion.org>
2022-04-17 16:30:55 +0100

    Consistently use sidenote in CSS class names

--- src/templates/notes/sidenote/index.tsx
+++ src/templates/notes/sidenote/index.tsx
@@ -5,10 +5,10 @@ import * as styles from "./index.module.css"
 const Sidenote = ({ children }) => {
   const inputId = React.useId()
   return (
-    <aside className={styles.footnoteContainer}>
-      <label className={styles.footnoteNumber} htmlFor={inputId}></label>
+    <aside className={styles.sidenoteContainer}>
+      <label className={styles.sidenoteNumber} htmlFor={inputId}></label>
       <input type="checkbox" className={styles.marginToggle} id={inputId} />
-      <span className={styles.footnote}>{children}</span>
+      <span className={styles.sidenote}>{children}</span>
     </aside>
   )
 }
